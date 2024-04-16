cbfb71e0e96b6286dede15ea1f0db0c71c5861da
aseel-saad <aseel.madhloom@gmail.com>
2024-04-16 12:44:37 +0300

    Security

---
+++ src/main/java/com/aseel/security/user/Role.java
@@ -0,0 +1,5 @@
+package com.aseel.security.user;
+
+public enum Role {
+    USER,ADMIN
+}
---
+++ src/test/java/com/aseel/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aseel.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
